53236928ec7520e492099f2b8ace16d77dab0214
Shubham Singhal <singhal121997@gmail.com>
2020-11-23 19:37:18 +0530

    add model viewset with custom function

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+from blog.models import BlogModel
+from blog.serializers import BlogSerializer
+
+
+class BlogViewset(viewsets.ModelViewSet):
+	queryset = BlogModel.objects.all()
+	serializer_class = BlogSerializer
+
+	@action(detail=True, methods=['get'])
+	def cool_blogs(self, request, pk=None):
+		blogs = BlogModel.objects.filter(pk=pk)
+		sr = BlogSerializer(blogs, many=True)
+		return Response({'data': sr.data})
